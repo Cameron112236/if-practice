b0d6ffbd93d8db7ceead278ac132f2b688a67b9c
Cameron <Camesmal179@ed.amdsb.ca>
2021-05-05 13:54:27 -0400

    Add project files.

---
+++ if practice/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int tendered = Convert.ToInt32(value: textBox1.Text);
+            double o = tendered % 7;
+            if (o != 0)
+            {
+
+                label1.Text = $"{tendered} is not divisible by 7 ";
+
+            }
+               else
+            {
+                label1.Text = $"{tendered} is divisible by 7";
+            }
+            if (tendered > 0)
+            {
+                label2.Text = $"{tendered} is a positive number";
+            }
+            else label2.Text = $"{tendered} is a negative number";
+        }
+    }
+}
